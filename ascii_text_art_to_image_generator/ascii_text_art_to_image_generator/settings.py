c534cf515144e21fd62bda78c0e70e1c69e250eb
Chooi Je Qin <42904912+jeqinchooi@users.noreply.github.com>
2021-07-19 22:34:08 +0800

    Modified allowed host

--- ascii_text_art_to_image_generator/ascii_text_art_to_image_generator/settings.py
+++ ascii_text_art_to_image_generator/ascii_text_art_to_image_generator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^-m_!7n!z9aw$^qqifstn=h50x*6w&gh^ex+x4=bzo84ll%lmr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
