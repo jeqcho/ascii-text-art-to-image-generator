6343468bc87352b74735ec73edfae7948407e6aa
Chooi Je Qin <42904912+jeqinchooi@users.noreply.github.com>
2021-07-22 17:11:00 +0800

    Removed static files from url

--- ascii_text_art_to_image_generator/ascii_text_art_to_image_generator/urls.py
+++ ascii_text_art_to_image_generator/ascii_text_art_to_image_generator/urls.py
@@ -7,5 +7,4 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('ascii-to-image/', include('ascii_to_image.urls')),
                   path('admin/', admin.site.urls),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
-                                                                                         document_root=settings.STATIC_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
