29e17f3e7c6ea4bc129c30257137d1b48beb156f
Chooi Je Qin <42904912+jeqinchooi@users.noreply.github.com>
2021-07-22 14:23:04 +0800

    Added more external links

--- ascii_text_art_to_image_generator/ascii_text_art_to_image_generator/settings.py
+++ ascii_text_art_to_image_generator/ascii_text_art_to_image_generator/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^-m_!7n!z9aw$^qqifstn=h50x*6w&gh^ex+x4=bzo84ll%lmr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'jeqcho.pythonanywhere.com']
 
